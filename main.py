93e8f86b77ebb429c322319f8d7a82c0988d8a2d
Volzhentsev Sergey <volzhentsev@bk.ru>
2021-11-03 15:56:58 +0300

    file main.py modified

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-import os
-from pprint import pprint
-path = os.path.join(os.getcwd(), 'data.txt')
-with open(path, encoding='utf-8') as file:
-    cook_book = {}
-    for dish in file:
-        dish_name = dish.strip()
-        counter = int(file.readline().strip())
-        temp_data = []
-        for item in range(counter):
-            ingredient_name, quantity, measure = file.readline().split('|')
-            temp_data.append({'ingredient_name': ingredient_name.strip(), 'quantity': quantity.strip(), 'measure': measure.strip()})
-        cook_book[dish_name] = temp_data
-        file.readline()
-    pprint(cook_book)
+def get_cook_book_dict(file_name):
+    import os
+    from pprint import pprint
+    path = os.path.join(os.getcwd(), file_name)
+    with open(path, encoding='utf-8') as file:
+        cook_book = {}
+        for dish in file:
+            dish_name = dish.strip()
+            counter = int(file.readline().strip())
+            temp_data = []
+            for item in range(counter):
+                ingredient_name, quantity, measure = file.readline().split('|')
+                temp_data.append({'ingredient_name': ingredient_name.strip(), 'quantity': quantity.strip(), 'measure': measure.strip()})
+            cook_book[dish_name] = temp_data
+            file.readline()
+        pprint(cook_book)
 
-
-    def get_shop_list_by_dishes(dishes, person_count):
-        shop_list = {}
-        for el_1 in dishes:
-            for el_2 in cook_book[el_1]:
-                if el_2['ingredient_name'] in shop_list.keys():
-                    shop_list[el_2['ingredient_name']]['quantity'] += int(el_2['quantity']) * person_count
-                else:
-                    shop_list[el_2['ingredient_name']] = {}
-                    shop_list[el_2['ingredient_name']]['measure'] = el_2['measure']
-                    shop_list[el_2['ingredient_name']]['quantity'] = int(el_2['quantity']) * person_count
-        print(shop_list)
-
-    get_shop_list_by_dishes(['Омлет', 'Омлет'], 3)
+get_cook_book_dict('data.txt')
 
 
 
