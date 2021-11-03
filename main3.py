ee3135d59281a9834b40db92f2b2ea945de6fead
Volzhentsev Sergey <volzhentsev@bk.ru>
2021-11-03 15:48:16 +0300

    file main3.py modified

--- main3.py
+++ main3.py
@@ -8,14 +8,14 @@ for file in files:
         with open(file, encoding='utf-8') as f:
             lines = f.readlines()
             data[file] = len(lines), lines
-# print(data)
 sorted_tuple = sorted(data.items(), key=lambda x: x[1])
-# print(sorted_tuple)
 for el in sorted_tuple:
     with open('united', 'a', encoding='utf-8') as f:
         f.write(str(el[0])+'\n')
         f.write(str(el[1][0])+'\n')
-        f.write(str(el[1][1])+'\n')
+        for el_2 in el[1][1]:
+            f.write(str(el_2))
+        f.write('\n')
 
 
 
